6b0a83be4546998ced7c0f109b67d7e5521f386c
stixlee <michael.lee.1983@iclllud.com>
2023-12-27 09:53:28 -0700

    Added dayString computed property to Date

--- Sources/FoundationSupplement/Date+Extensions.swift
+++ Sources/FoundationSupplement/Date+Extensions.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+public extension Date {
+    
+    var dayString: String {
+        let weekdays = [
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday"
+        ]
+        
+        let calendar = Calendar.current.component(.weekday, from: self)
+        return weekdays[calendar - 1]
+    }
+}
